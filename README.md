612ccfde096adaaf29de24dce7c73550d2efa5b0
S Mautner <mautner@cs.uni-freiburg.de>
2021-05-19 10:19:58 +0200

    remake

---
+++ README.md
@@ -0,0 +1,26 @@
+
+# Usage:
+
+```
+from dirtyopts import parse
+
+doc = '''
+#--argname type[+ for list] [default:defaultarg]  [doublespace -> comment]
+--intarg int+ default:3 4 5  i am a comment
+
+# nodefault means the default of the type 
+--strarg str  ->''
+
+# you need to provide a string for bool arguments because where is no special treatment for them
+>>>myprog --boolarg True   
+'''
+
+doc2 = '''
+# i ignore unknown args so multiple args opbjects can be created neatly
+--intargford2 int
+'''
+
+args = parse(doc)
+args2 = parse(doc2)
+```
+
--- dirtyopts/opts.py
+++ dirtyopts/opts.py
@@ -1,76 +1,119 @@
 import re
 import sys
-
+import logging
 doc='''
 i guess we write stuff like so:
 NAMEOFTHING [TYPE] [default: DEFAULT]  [DOUBLESPACE DESCRIPTION]
 
 --myintarg int default:12   blabla
---something    we will assume that it is a bool i think
 --mystring str default:asd  blabla
---myfu eval default: lambda x:x 
+--myfu eval default:lambda x:x 
 --another bool default:True
+--zomg int+
+--anotherf bool+ default:False False
 '''
 
+
+
+
+######################
+# READ THE DOCSTRING 
+####################
+
+
 #REGEX:
-#0:name
-#1:type
-#2: NOTHIG
-#3:defaultvalue
-getgroups = re.compile( '--([\w]+) ?([\w]+)? ?(default:(.+))? ?')
+#1:name
+#2:type
+#3:list?
+#4: NOTHIG
+#5:defaultvalue
+getgroups = re.compile( '--([\w]+) ?(\w+)?(\+)? ?(default:(.+))? ?')
+
+
+def interpret_groups(argname, maker, islist, default, defaults, funcs):
+    if maker == 'bool':
+        makerf=lambda x: x=='True'
+    else:
+        makerf=eval(maker)
+
+    if maker == 'eval': 
+        funcs[argname] = lambda x: eval(" ".join(x))
+    elif islist:
+        funcs[argname]  = lambda x: list(map(makerf,x))
+    else:
+        funcs[argname]  = lambda x: makerf(x[0])
+    
+    if default:
+        defaults[argname] = funcs[argname](default.split())  
+    else:
+        defaults[argname] = makerf() if not islist else [makerf()]
+
+
+
 
 def docstrparser(docstring):
-    args={}
+    defaults={}
     argfun={}
     for line in docstring.split('\n'):
         line= line.strip()
         if line and line[:2]=='--':
             line = line.split("  ")[0]
             m=getgroups.match(line)
-            #print('grps',m.group(1),m.group(2),m.group(4))
-            # lets read the groups 
-            name = m.group(1)
-            argfun[name] = eval(m.group(2)) if m.group(2) else bool # set type
-            args[name] = argfun[name](m.group(4))   # bool(None) is false so this is fine
+            interpret_groups(*[m.group(x) for x in [1,2,3,5]], defaults, argfun)
+    # bool(None) is false so this is fine
+    return defaults, argfun
 
-    return args, argfun
 
+##############
+#  READ THE ARGS
+#############
+def app(res,carg,cstuff):
+    if not cstuff:
+        logging.warning(f'arg missing: {carg}')
+    res[carg] = cstuff
+    return []
 
 def argparser(args):
     '''
-    returns {arg:string}
+    returns {argname:list}
     '''
     result = {}
     carg = None
-    cstuff = ''
+    cstuff = []
     for e in args:
         if e[:2]== '--':
             if carg:
-                result[carg] = cstuff.strip()
-                cstuff = ''
+                cstuff = app(result,carg, cstuff)
             carg = e[2:]
         else:
-            cstuff+=e
-    if carg:result[carg] = cstuff.strip()
+            cstuff.append(e)
+    if carg:
+        cstuff = app(result,carg, cstuff)
     return result
 
+
+
+############3
+# READ THE ARGS
+#############
 class argz:
     def __init__(self,stuff):
         self.__dict__.update(stuff)
 
-def parse( docstring , args =  sys.argv[1:]):
-    defaultargs, argfun = docstrparser(docstring)
+
+def parse(docstring , args =  sys.argv[1:]):
     rawargs = argparser(args)
+    defaultargs, argfun = docstrparser(docstring)
+
 
     for arg,v in rawargs.items():
-        if arg in defaultargs:
-            if v == '':
-                defaultargs[arg] = not defaultargs[arg]
-            elif arg in argfun:
-                defaultargs[arg] = argfun[arg](v)
-
+        if arg not in argfun:
+            logging.warningt (f"this docstring doesnt handle: {arg}")
+        else:
+            defaultargs[arg] = argfun[arg](v)
 
     return argz(defaultargs)
 
 if __name__ == '__main__':
     print(parse(doc).__dict__)
+
